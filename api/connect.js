85038a569de1307c9ee54b111abacf894474c8ea
0xZales <rozalesamiral@gmail.com>
2023-03-31 16:54:08 +0000

    database update

--- api/connect.js
+++ api/connect.js
@@ -1,22 +1,13 @@
 import dotenv from "dotenv";
 import mysql from 'mysql'
 dotenv.config();
-var connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
-})
+
 export const connectToDb=  ()=>{
-  return connection.connect(function (err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    } else {
-      console.log("Connexion a mysql reussie")
-    }
+  const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
   })
-}
-export const closeConnection = ()=>{
-  return connection.end(()=>console.log("Deconnexion reussie"))
+  return  connection
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,36 @@
 import express from "express";
-import { connectToDb, closeConnection } from "../connect.js";
+import dotenv from "dotenv";
+import CryptoJS from "crypto-js";
+import { connectToDb } from "../connect.js";
+dotenv.config();
+
 const router = express.Router();
 router.post('/create', async (request, response) => {
-    const name = request.body.name
-    const lastName = request.body.lastName
-    console.log(name + lastName)
-
+    // Recuperation des information pour creer un user
+    const pass = request.body.mot_de_passe
+    // Crypage de password
+    const encryptedPassord = CryptoJS.AES.encrypt(pass, process.env.CRYPTAGE).toString();
+    // Définir les données à insérer
+    const newUser = {
+        nom: request.body.nom,
+        prenom: request.body.prenom,
+        email: request.body.email,
+        mot_de_passe: encryptedPassord
+    }
+    // Requête d'insertion dans la table "user"
+    const query = 'INSERT INTO user SET ?';
+    // connexiona la DB
+   const db= connectToDb()
+   db.connect()
+    // Exécuter la requête avec les données à insérer
+    db.query(query, newUser, (error, results, fields) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('Nouvel utilisateur inséré avec succès.');
+        }
+      });
+    response.status(201).json({ passBefor: newUser.mot_de_passe, passAfter: encryptedPassord });
 })
 
 export default router
