8e9a0f13024950571ac2a953933ed3dd4d37a4f9
unknown <valerienhounou@gmail.com>
2023-03-31 16:04:53 +0000

    commentaire

--- api/index.js
+++ api/index.js
@@ -2,14 +2,14 @@ import express from "express";
 import userRouter from './routes/user.js'
 // Initialisation de l'app
 var app = express();
-app.use(express.json());
-const port = 5000
-app.listen(port,  () => {
+app.use(express.json());//pour recevoir des requetes en Json
+const port = 5000//déclaration du port du serveur 
+app.listen(port,  () => {// Rhozales a dit: "mets toi en ecoute sur ce port là" et si c'est bon affiche le message
     console.log("Serveur Express connecté sur le port " + port)
 })
 
 // route index
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {//route de la page principale une fois la connection établie
     res.send('Bienvenue sur Task.com');
 });
-app.use('/task/v1',userRouter)
+app.use('/task/v1',userRouter)//ça c'est ce qui s'affichera lorsqu'un utilisateur cliquera sur notre lien  + la reference donné dans router de user
